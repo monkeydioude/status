d21ed111db928f344b54f0c0f277b717b5bff291
mkd <monkeydioude@gmail.com>
2024-07-28 18:15:13 +0200

    no line

--- internal/service/service_linux.go
+++ internal/service/service_linux.go
@@ -3,7 +3,7 @@ package service
 import "os/exec"
 
 func GetServiceStatus(serviceName string) (string, error) {
-	cmd := exec.Command("systemctl", "status", serviceName)
+	cmd := exec.Command("systemctl", "status", serviceName, "-n", "0")
 	output, err := cmd.Output()
 	if err != nil {
 		return "", err
