d5b427b2872d342122618ad10aedf320e7f5a855
mkd <monkeydioude@gmail.com>
2024-07-28 20:00:30 +0200

    no need for pre's pre treatment

--- internal/service/health.go
+++ internal/service/health.go
@@ -1,9 +1,5 @@
 package service
 
-import (
-	"regexp"
-)
-
 type ServiceHealth struct {
 	Name    string `json:"name"`
 	Health  string `json:"health"`
@@ -16,18 +12,6 @@ func (s *ServiceHealth) ProvideSystemctlStatus() error {
 	if err != nil {
 		return err
 	}
-	strToReplace := s.Name + "\\["
-	m := regexp.MustCompile(strToReplace)
-	replace := "\n\n" + s.Name + "["
-	status = m.ReplaceAllString(status, replace)
-	// loc := m.FindStringIndex(status)
-	// fmt.Println("loc", loc)
-	// if loc != nil {
-	// 	before := status[:loc[0]]
-	// 	after := status[loc[1]:]
-	// 	replaced := strings.Replace(status[loc[0]:loc[1]], strToReplace, "<pre>"+strToReplace, 1)
-	// 	status = before + replaced + after + "</pre>"
-	// }
 	s.Status = status
 	return nil
 }
