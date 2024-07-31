1e9295f5f76d0a12556c5a922c10129bf5dbf761
mkd <monkeydioude@gmail.com>
2024-07-31 19:21:31 +0200

    some comments and ensure OK requires a 200 status code only

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -14,20 +14,27 @@ import (
 func Index(config []status.Config) func(*gin.Context) {
 	return func(c *gin.Context) {
 		healthchecks := make([]service.ServiceHealth, 0)
+		// looping through the service list
 		for _, serviceConf := range config {
 			health := service.ServiceHealth{
-				Name: serviceConf.Name,
+				Name:   serviceConf.Name,
+				Health: "KO",
 			}
+			// trying to fetch the task's status.
+			// works only for linux atm.
 			if serviceConf.Daemon {
 				if err := health.ProvideSystemctlStatus(); err != nil {
 					health.Message = err.Error()
 				}
 			}
+			// prepend with http:// if no scheme is provided
 			url := serviceConf.HealthcheckUrl
 			if !strings.HasPrefix(url, "http") {
 				url = "http://" + serviceConf.HealthcheckUrl
 			}
+			// requesting the healthcheck endpoint
 			res, err := http.Get(url)
+			// should actually answering KO if there's an issue with the request
 			if err != nil {
 				healthchecks = append(healthchecks, service.ServiceHealth{
 					Name:    serviceConf.Name,
@@ -36,27 +43,32 @@ func Index(config []status.Config) func(*gin.Context) {
 				})
 				continue
 			}
+			// reading the body
 			body, err := io.ReadAll(res.Body)
+			// should not answer KO if there's an with reading the body.
 			if err != nil {
 				healthchecks = append(healthchecks, service.ServiceHealth{
 					Name:    serviceConf.Name,
-					Health:  "KO",
 					Message: health.Message + "\n" + err.Error(),
-					// Message: err.Error(),
 				})
-				continue
 			}
 			err = json.Unmarshal(body, &health)
+			// should not answer KO if there's an issue with unmarshalling.
 			if err != nil {
 				healthchecks = append(healthchecks, service.ServiceHealth{
-					Name:    serviceConf.Name,
-					Health:  "KO",
-					Message: err.Error(),
+					Name: serviceConf.Name,
 				})
-				continue
+			}
+
+			// a status code 200 is all that matters to be OK
+			if res.StatusCode != 200 {
+				health.Health = "KO"
+			} else {
+				health.Health = "OK"
 			}
 			healthchecks = append(healthchecks, health)
 		}
+		// render the HTML template
 		c.HTML(http.StatusOK, "index.tmpl", healthchecks)
 	}
 }
