c0495a84bf3cb21e0904eb03435b73ef9c784e20
mkd <monkeydioude@gmail.com>
2024-10-17 22:30:33 +0200

    trying parallel query of systemctl in goroutines

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -7,6 +7,7 @@ import (
 	"status/internal/service"
 	"status/internal/status"
 	"strings"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,65 +21,71 @@ const (
 func Index(config []status.Config) func(*gin.Context) {
 	return func(c *gin.Context) {
 		healthchecks := make([]service.ServiceHealth, 0)
+		var wg sync.WaitGroup
 		// looping through the service list
 		for _, serviceConf := range config {
-			health := service.ServiceHealth{
-				Name:   serviceConf.Name,
-				Health: KO,
-			}
-			// trying to fetch the task's status.
-			// works only for linux atm.
-			if serviceConf.Daemon {
-				if err := health.ProvideSystemctlStatus(); err != nil {
-					health.Message = err.Error()
+			wg.Add(1)
+			go func(serviceConf status.Config) {
+				defer wg.Done()
+				health := service.ServiceHealth{
+					Name:   serviceConf.Name,
+					Health: KO,
 				}
-			}
-			// prepend with http:// if no scheme is provided
-			url := serviceConf.HealthcheckUrl
-			if !strings.HasPrefix(url, "http") {
-				url = "http://" + serviceConf.HealthcheckUrl
-			}
-			// requesting the healthcheck endpoint
-			res, err := http.Get(url)
-			// should actually answering KO if there's an issue with the request
-			if err != nil {
-				healthchecks = append(healthchecks, service.ServiceHealth{
-					Name:    serviceConf.Name,
-					Health:  KO,
-					Message: health.Message + "\n" + err.Error(),
-				})
-				continue
-			}
-			// reading the body
-			body, err := io.ReadAll(res.Body)
-			// should not answer KO if there's an with reading the body.
-			if err != nil {
-				healthchecks = append(healthchecks, service.ServiceHealth{
-					Name:    serviceConf.Name,
-					Health:  NANI,
-					Message: health.Message + "\n" + err.Error(),
-				})
-				continue
-			}
-			err = json.Unmarshal(body, &health)
-			// should not answer KO if there's an issue with unmarshalling.
-			if err != nil {
-				healthchecks = append(healthchecks, service.ServiceHealth{
-					Name:    serviceConf.Name,
-					Health:  NANI,
-					Message: health.Message + "\n" + err.Error(),
-				})
-				continue
-			}
+				// trying to fetch the task's status.
+				// works only for linux atm.
+				if serviceConf.Daemon {
+					if err := health.ProvideSystemctlStatus(); err != nil {
+						health.Message = err.Error()
+					}
+				}
+				// prepend with http:// if no scheme is provided
+				url := serviceConf.HealthcheckUrl
+				if !strings.HasPrefix(url, "http") {
+					url = "http://" + serviceConf.HealthcheckUrl
+				}
+				// requesting the healthcheck endpoint
+				res, err := http.Get(url)
+				// should actually answering KO if there's an issue with the request
+				if err != nil {
+					healthchecks = append(healthchecks, service.ServiceHealth{
+						Name:    serviceConf.Name,
+						Health:  KO,
+						Message: health.Message + "\n" + err.Error(),
+					})
+					return
+				}
+				// reading the body
+				body, err := io.ReadAll(res.Body)
+				// should not answer KO if there's an with reading the body.
+				if err != nil {
+					healthchecks = append(healthchecks, service.ServiceHealth{
+						Name:    serviceConf.Name,
+						Health:  NANI,
+						Message: health.Message + "\n" + err.Error(),
+					})
+					return
+				}
+				err = json.Unmarshal(body, &health)
+				// should not answer KO if there's an issue with unmarshalling.
+				if err != nil {
+					healthchecks = append(healthchecks, service.ServiceHealth{
+						Name:    serviceConf.Name,
+						Health:  NANI,
+						Message: health.Message + "\n" + err.Error(),
+					})
+					return
+				}
 
-			// a status code 200 is all that matters to be OK
-			if res.StatusCode != 200 {
-				health.Health = KO
-			} else {
-				health.Health = OK
-			}
-			healthchecks = append(healthchecks, health)
+				// a status code 200 is all that matters to be OK
+				if res.StatusCode != 200 {
+					health.Health = KO
+				} else {
+					health.Health = OK
+				}
+				healthchecks = append(healthchecks, health)
+			}(serviceConf)
 		}
+		wg.Wait()
 		// render the HTML template
 		c.HTML(http.StatusOK, "index.tmpl", healthchecks)
 	}
